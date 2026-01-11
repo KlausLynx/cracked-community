5b8bea50d21d8ac296a420628273472469a6a2a8
KlausLynx <onyiavitus@gmail.com>
2026-01-11 14:34:01 +0100

    `Added React Router and Lucide React to the project`

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
 import LandingPage from './Landing'
+import LoginPage from './Login'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 function App() {
 
   return (
-    <>
-     <LandingPage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LandingPage/>}></Route>
+        <Route path='/Login.jsx' element={<LoginPage/>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,22 +1,38 @@
+import { ArrowBigRight, UserRound, CircleUserRoundIcon } from "lucide-react"
+import { Link } from "react-router-dom"
 export default function LandingPage() {
     return (
         <div className="grid grid-rows-2 md:grid-cols-2 md:grid-rows-1 h-screen">
             <div className="bg-indigo-800">
 
             </div>
-            <div>
-                <div>
-                     <p>Already have an Account <strong>Login Here</strong></p>
+            <div className="md:pl-6 pr-2 flex flex-col"> 
+                <div className="flex justify-end pt-2">
+                     <p>Already have an Account <Link to="/Login.jsx"><strong>Login Here</strong></Link></p>
                 </div>
                
-                <div>
+                <div className="max-w-[460px] p-4 flex flex-col flex-1 gap-7 items-start justify-center">
                     <div>
-                        <h1 className="text-xl font-bold">Join us!</h1>
-                        <p>To begin this Journey, tell us how you plan to start</p>
+                        <h1 className="text-xl md:text-2xl font-bold">Join Us!</h1>
+                        <p className="text-md text-lg text-gray-400 font-bold">To begin this Journey, tell us how<br/> you plan to start</p>
                     </div>
 
-                    <form action="">
+                    <form action="" className="flex flex-col gap-y-10 w-full">
+                        <div className="flex items-center gap-3 border-2 border-indigo-800 px-4 py-3 rounded-2xl">
+                            <UserRound className="text-indigo-600" size={40} />
+                            <div className="max-w-[305px] flex-1">
+                                <p className="text-gray-400 font-medium" >New here? Sign up to start accessing</p>
+                            </div>
+                            <Link to="/src/Signup.jsx"><ArrowBigRight className="text-indigo-600" /></Link>   
+                        </div>
 
+                        <div className="flex items-center gap-3 border-2 border-indigo-800 px-4 py-3 rounded-2xl">
+                            <CircleUserRoundIcon className="text-indigo-600" size={40} />
+                           <div className="max-w-[305px] flex-1">
+                            <p className="text-gray-400 font-medium">Already have access? Log into your secure account to view your saved materials.</p>
+                           </div>
+                            <Link to="/src/Login.jsx"><ArrowBigRight className="text-indigo-600" /></Link>   
+                        </div>
                     </form>
                 </div>
 
